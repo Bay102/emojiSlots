86fb4a3fa7d61dc27800cd27cb3646851ba37977
Zak Bay <zacharybay@live.com>
2023-01-10 20:52:02 -0800

    Randomized Accomplished, Not DRY

--- src/EmojiSlot.jsx
+++ src/EmojiSlot.jsx
@@ -1,56 +1,79 @@
 import React from "react";
-// each button rest to a random value on click 
+// each button rest to a random value on click
 class EmojiSlot extends React.Component {
-   constructor() {
-      super();
-      this.emojis1 = ['🥺', '💩', '🤑', '🎰', '🇺🇸', '🙈', '😏', '💣',];
-      this.emojis2 = ['🤑', '💣', '🤬', '🤑', '🤬', '🇺🇸', '👺', '🙈',];
-      this.emojis3 = ['💩', '🥺', '🇺🇸', '🎰', '😏', '🤬', '💣', '🥺',];
+  constructor() {
+    super();
+    this.emojis = ["🥺", "💩", "🤑", "🎰", "🇺🇸", "🙈", "😏", "💣", "☘️"];
 
-      this.state = {
-         emojiSet1: '',
-         emojiSet2: '',
-         emojiSet3: '',
-      }
-   }
+    this.state = {
+      emojiSet1: "",
+      emojiSet2: "",
+      emojiSet3: "",
+      emojiSet4: "",
+      emojiSet5: "",
+      emojiSet6: "",
+      emojiSet7: "",
+      emojiSet8: "",
+      emojiSet9: "",
+    };
+  }
 
-   spin = () => {
-    const i = Math.floor(Math.random() * this.emojis1.length);
-    this.setState(
-      {
-         emojiSet1: this.emojis1[i],
-         emojiSet2: this.emojis2[i],
-         emojiSet3: this.emojis3[i],
-      })
-    }
+  randomize = (emojiArray) => {
+    const i = Math.floor(Math.random() * emojiArray.length);
+    return emojiArray[i];
+  };
 
+  spin = () => {
+    this.setState({
+      emojiSet1: this.randomize(this.emojis),
+      emojiSet2: this.randomize(this.emojis),
+      emojiSet3: this.randomize(this.emojis),
+      emojiSet4: this.randomize(this.emojis),
+      emojiSet5: this.randomize(this.emojis),
+      emojiSet6: this.randomize(this.emojis),
+      emojiSet7: this.randomize(this.emojis),
+      emojiSet8: this.randomize(this.emojis),
+      emojiSet9: this.randomize(this.emojis),
+    });
+  };
 
-   render() {
-           const {emojiSet1, emojiSet2, emojiSet3} = this.state
-      return (
-        
-         <div className="gameBox">
-          <div className="row row1">
-               <div className="box box1">{emojiSet3}</div>
-               <div className="box box2">{emojiSet2}</div>
-               <div className="box box3">{emojiSet1}</div>
-            </div>
-             <div className="row row2">
-               <div className="box box4">{emojiSet1}</div>
-               <div className="box box5">{emojiSet2}</div>
-               <div className="box box6">{emojiSet3}</div>
-            </div>
-             <div className="row row3">
-               <div className="box box7">{emojiSet2}</div>
-               <div className="box box8">{emojiSet3}</div>
-               <div className="box box9">{emojiSet1}</div>
-            </div>
-            <div className="spinWrapper">
-               <button onClick={this.spin} className="spinButton">CLICK TO SPIN</button>
-            </div>
-         </div>
-      )
-   }
+  render() {
+    const {
+      emojiSet1,
+      emojiSet2,
+      emojiSet3,
+      emojiSet4,
+      emojiSet5,
+      emojiSet6,
+      emojiSet7,
+      emojiSet8,
+      emojiSet9,
+    } = this.state;
+    return (
+      <div className="gameBox">
+        <div className="row row1">
+          <div className="box box1">{emojiSet1}</div>
+          <div className="box box2">{emojiSet2}</div>
+          <div className="box box3">{emojiSet3}</div>
+        </div>
+        <div className="row row2">
+          <div className="box box4">{emojiSet4}</div>
+          <div className="box box5">{emojiSet5}</div>
+          <div className="box box6">{emojiSet6}</div>
+        </div>
+        <div className="row row3">
+          <div className="box box7">{emojiSet7}</div>
+          <div className="box box8">{emojiSet8}</div>
+          <div className="box box9">{emojiSet9}</div>
+        </div>
+        <div className="spinWrapper">
+          <button onClick={this.spin} className="spinButton">
+            CLICK TO SPIN
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default EmojiSlot ;
+export default EmojiSlot;
